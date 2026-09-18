87c27c62e21678b288913d813bd3021fbd036461
Agastya <krishkhati0007king@gmail.com>
2026-09-18 20:14:24 +0530

    Add Mico marketing landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { redirect } from "next/navigation";
+import MarketingLanding from "./marketing-landing";
 
 export default function Home() {
-  redirect("/login");
-  return null;
+  return <MarketingLanding />;
 }
